11da739224fc7f81d3e7aaaf2dd2fa17a0421c25
RyuBAI <me@ryubai.com>
2021-01-24 21:53:07 +0800

    bruh

--- tools/decrypt.js
+++ tools/decrypt.js
@@ -1,15 +1,27 @@
+
+//---Please retrieve this first-------------------------------------------------------
+
+var decrypt_key = ""
+
+//------------------------------------------------------------------------------------
+
 const crypto = require("crypto")
 const path = require("path")
 const fs = require("fs")
 
-var decrypt_key = ""
+var log_path = path.join(__dirname, '\\encryption_info.log.json')
 
-decrypt = (plugins) => {
-    const i = plugins.slice(0,16)
-    plugins = plugins.slice(16)
-    const d = crypto.createDecipheriv("aes-256-ctr", decrypt_key, i)
-    const r = Buffer.concat([d.update(plugins), d.final()])
-    return r
+decrypt = (data) => {
+    var iv = data.slice(0,16)
+    data = data.slice(16)
+    var d = crypto.createDecipheriv("aes-256-ctr", decrypt_key, iv)
+    return Buffer.concat([d.update(data), d.final()])
+}
+
+encrypt = (data, ivHex) => {
+    var iv = Buffer.from(ivHex, 'hex')
+    var e = crypto.createCipheriv("aes-256-ctr", decrypt_key, iv)
+    return Buffer.concat([iv, e.update(data), e.final()])
 }
 
 findExt = (base, ext, files, result) => {
@@ -23,29 +35,63 @@ findExt = (base, ext, files, result) => {
     return result
 }
 
+fileListWithIVInfo = (filePathList) => {
+    result = []
+    for(let i=0; i<filePathList.length; i++){
+        var buff = fs.readFileSync(filePathList[i])
+        var iv = buff.slice(0,16)
+        result.push({path : filePathList[i], iv : iv.toString('hex')})
+    }
+    return result
+}
+
 findAllEncrypted = () => {
     var encrypt = [
-        { extension : 'OMORI' , real_extension : 'js'   , file_list : findExt(path.join(__dirname, '\\..\\'),'OMORI' )},
-        { extension : 'KEL'   , real_extension : 'json' , file_list : findExt(path.join(__dirname, '\\..\\'),'KEL'   )},
-        { extension : 'HERO'  , real_extension : 'yaml' , file_list : findExt(path.join(__dirname, '\\..\\'),'HERO'  )},
-        { extension : 'AUBREY', real_extension : 'json' , file_list : findExt(path.join(__dirname, '\\..\\'),'AUBREY')},
-        { extension : 'PLUTO' , real_extension : 'yaml' , file_list : findExt(path.join(__dirname, '\\..\\'),'PLUTO' )},
+        { extension : 'OMORI' , real_extension : 'js'   , file_list : fileListWithIVInfo(findExt(path.join(__dirname, '\\..\\'),'OMORI' ))},
+        { extension : 'KEL'   , real_extension : 'json' , file_list : fileListWithIVInfo(findExt(path.join(__dirname, '\\..\\'),'KEL'   ))},
+        { extension : 'HERO'  , real_extension : 'yaml' , file_list : fileListWithIVInfo(findExt(path.join(__dirname, '\\..\\'),'HERO'  ))},
+        { extension : 'AUBREY', real_extension : 'json' , file_list : fileListWithIVInfo(findExt(path.join(__dirname, '\\..\\'),'AUBREY'))},
+        { extension : 'PLUTO' , real_extension : 'yaml' , file_list : fileListWithIVInfo(findExt(path.join(__dirname, '\\..\\'),'PLUTO' ))},
     ]
-    fs.writeFileSync(path.join(__dirname, '\\encrypted.log.json'), JSON.stringify({encrypt}, null, 4))
     return encrypt
 }
 
 decryptAll = () => {
-    let encryptedList = findAllEncrypted()
+    if(decrypt_key === '') { console.log('Please extract decrypt key first!'); return }
+
+    console.log('Finding encrypted files...')
+    var encryptedList = findAllEncrypted()
+    fs.writeFileSync(log_path, JSON.stringify(encryptedList, null, 4))
+
+    console.log('Decrypting...')
     encryptedList.forEach((encryptType) => {
-        encryptType.file_list.forEach((filePath) => {
-            console.log(filePath)
-            let buff = fs.readFileSync(filePath)
-            let res = decrypt(buff).toString()
-            fs.writeFileSync(filePath+'.decipher.'+encryptType.real_extension, res)
+        encryptType.file_list.forEach((fileInfo) => {
+            var decryptedPath = fileInfo.path + '.decipher.' + encryptType.real_extension
+            var buff = fs.readFileSync(fileInfo.path)
+            var res = decrypt(buff).toString()
+            fs.writeFileSync(decryptedPath, res)
         })
     })
+    console.log('Done!')
 }
 
-if(decrypt_key === '') { console.log('Please extract decrypt key first!') }
-else decryptAll()
+encryptAndOverwriteAll = () => {
+    if(decrypt_key === '') { console.log('Please extract decrypt key first!'); return }
+
+    if(!fs.existsSync(log_path)) { console.log('Please decrypt first!'); return }
+    var encryptedList = JSON.parse(fs.readFileSync(log_path))
+
+    console.log('Encrypting...')
+    encryptedList.forEach((encryptType) => {
+        encryptType.file_list.forEach((fileInfo) => {
+            var decryptedPath = fileInfo.path + '.decipher.' + encryptType.real_extension
+            var buff = fs.readFileSync(decryptedPath)
+            var res = encrypt(buff, fileInfo.iv)
+            fs.writeFileSync(fileInfo.path, res)
+        })
+    })
+    console.log('Done!')
+}
+
+
+decryptAll()
